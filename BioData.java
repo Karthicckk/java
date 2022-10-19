028f2a5185f5199b8a255fe669627374ef9c38d1
Karthicckk <115642228+Karthicckk@users.noreply.github.com>
2022-10-19 18:42:28 +0530

    Update BioData.java

--- BioData.java
+++ BioData.java
@@ -2,10 +2,10 @@ public class BioData {
     public static void main(String[] args) {
     
 	String Name= "Karthick";
-	String Gender= "Male";
+	String Gender= "M";
 	String DOB="31/05/1995";
 	int Age=27;
-	String Maritalstatus="Unmarried";
+	String Maritalstatus="No";
 	String Doornumber="43";
 	String Street="Palaniyappa Nagar";
 	String Locality="Junction";
@@ -15,31 +15,31 @@ public class BioData {
 	int Pincode=636005;
 	String IsAadharAvailable="Yes";
 	String IsTelephoneAvailable="Yes";
-	long PhoneNumber=8220644993;
+	long PhoneNumber=8220644993L;
 	String Qualification="B.E.Mech";
 	String IsSalaried="Yes";
 	String MonthlySalary="25000";
 	String MonthlyExpenses="15000";
 	String Hobbies="Listening Music,Reading Books";
-	System.out.println("Name\t\t\t:"+Name);
-	System.out.println("Gender\t\t\t:"+Gender);
-	System.out.println("DOB\t\t\t:"+DOB);
-	System.out.println("Age\t\t\t:"+Age);
-	System.out.println("Marital status\t\t:"+Maritalstatus);
-	System.out.println("Door number\t\t:"+Doornumber);
-	System.out.println("Street\t\t\t:"+Street);
-	System.out.println("Locality\t\t:"+Locality);
-	System.out.println("City\t\t\t:"+City);
-	System.out.println("State\t\t\t:"+State);
-	System.out.println("Country\t\t\t:"+Country);
-	System.out.println("Pincode\t\t\t:"+Pincode);
+	System.out.println("Name\t\t\t\t\t:"+Name);
+	System.out.println("Gender\t\t\t\t\t:"+Gender);
+	System.out.println("DOB\t\t\t\t\t\t:"+DOB);
+	System.out.println("Age\t\t\t\t\t\t:"+Age);
+	System.out.println("Marital status\t\t\t:"+Maritalstatus);
+	System.out.println("Door number\t\t\t\t:"+Doornumber);
+	System.out.println("Street\t\t\t\t\t:"+Street);
+	System.out.println("Locality\t\t\t\t:"+Locality);
+	System.out.println("City\t\t\t\t\t:"+City);
+	System.out.println("State\t\t\t\t\t:"+State);
+	System.out.println("Country\t\t\t\t\t:"+Country);
+	System.out.println("Pincode\t\t\t\t\t:"+Pincode);
 	System.out.println("IsAadharAvailable       :"+IsAadharAvailable);
 	System.out.println("IsTelephoneAvailable    :"+IsTelephoneAvailable);
-	System.out.println("Phone Number\t\t:"+PhoneNumber);
-	System.out.println("Qualification\t\t:"+Qualification);
-	System.out.println("IsSalaried\t\t:"+IsSalaried);
-	System.out.println("Monthly Salary\t\t:"+MonthlySalary);
-	System.out.println("Monthly Expenses\t:"+MonthlyExpenses);
-	System.out.println("Hobbies\t\t\t:"+Hobbies);
+	System.out.println("Phone Number\t\t\t:"+PhoneNumber);
+	System.out.println("Qualification\t\t\t:"+Qualification);
+	System.out.println("IsSalaried\t\t\t\t:"+IsSalaried);
+	System.out.println("Monthly Salary\t\t\t:"+MonthlySalary);
+	System.out.println("Monthly Expenses\t\t:"+MonthlyExpenses);
+	System.out.println("Hobbies\t\t\t\t\t:"+Hobbies);
 }
 }
