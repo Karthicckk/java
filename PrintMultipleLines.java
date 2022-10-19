e5e441ff13e0fe81b8cf32efef9a1033b1898731
Karthicckk <115642228+Karthicckk@users.noreply.github.com>
2022-10-19 18:09:44 +0530

    Add files via upload

---
+++ PrintMultipleLines.java
@@ -0,0 +1,10 @@
+public class PrintMultipleLines
+
+	{
+	public static void main(String args[])
+	{
+	System.out.println("Have a nice day");
+	System.out.println("Everything is possible");
+	System.out.println("To be continue");
+	}
+}
---
+++ Variablesintro.java
@@ -0,0 +1,12 @@
+public class Variablesintro
+   {
+public static void main(String args[]) {
+int a=40;
+int b=60;
+int c=b;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+
+}
+}
