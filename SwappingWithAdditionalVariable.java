23e094c8b524f1329f2a76e1eed66ddbcb488233
Karthicckk <115642228+Karthicckk@users.noreply.github.com>
2022-10-20 18:11:48 +0530

    Add files via upload

---
+++ SwappingWithAdditionalVariable.java
@@ -0,0 +1,12 @@
+public class SwappingWithAdditionalVariable
+	{
+	public static void main(String args[]) {
+		int a=77;
+		int b=6598;
+		int c=254;
+		int d=b;
+		System.out.println("a="+d);
+		System.out.println("b="+c);
+		System.out.println("c="+a);
+	}
+	}
